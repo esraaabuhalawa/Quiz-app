01df81716f026e1cdda4959e62bbb31868172694
esraaabuhalawa <esr.saad123@gmail.com>
2026-07-17 23:41:10 +0300

    solve profile issues

--- src/app/shared/components/profile-dropdown/profile-dropdown.component.ts
+++ src/app/shared/components/profile-dropdown/profile-dropdown.component.ts
@@ -1,35 +1,35 @@
 import { Component, inject } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
+//import { AsyncPipe } from '@angular/common';
 import { Router, RouterLink } from "@angular/router";
-import { Skeleton } from 'primeng/skeleton';
+//import { Skeleton } from 'primeng/skeleton';
 import { Popover } from 'primeng/popover';
 
 import { TranslatePipe } from '@ngx-translate/core';
-import { RoleEnum } from '../../../core/enum/role.enum';
-import { ICurrentUser } from '../../../features/auth/interfaces/auth';
+// import { RoleEnum } from '../../../core/enum/role.enum';
+// import { ICurrentUser } from '../../../features/auth/interfaces/auth';
 import { AuthService } from '../../../features/auth/services/auth.service';
 @Component({
   selector: 'app-profile-dropdown',
-  imports: [AsyncPipe, RouterLink, Popover, Skeleton,TranslatePipe],
+  imports: [RouterLink, Popover,TranslatePipe],
   templateUrl: './profile-dropdown.component.html',
   styleUrl: './profile-dropdown.component.scss',
 })
 export class ProfileDropdownComponent {
   private readonly authService = inject(AuthService);
   private readonly router = inject(Router)
-  currentUser!: ICurrentUser | null;
-  currentUser$ = this.authService.currentUser$;
+ // currentUser!: ICurrentUser | null;
+  // currentUser$ = this.authService.currentUser$;
 
-  logout() {
-    this.authService.logout();
-    if (this.isAdmin) {
-      this.router.navigate(['/auth/login']);
-    }
-  }
+  // logout() {
+  //   this.authService.logout();
+  //   if (this.isAdmin) {
+  //     this.router.navigate(['/auth/login']);
+  //   }
+  // }
 
-  get isAdmin(): boolean {
-    return this.authService.getRole() === RoleEnum.Admin
-  }
+  // get isAdmin(): boolean {
+  //   return this.authService.getRole() === RoleEnum.Admin
+  // }
 
   //image Error
   onImageError(event: Event): void {
