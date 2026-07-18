2918450a2512ae24c34f40127ec2d618d41c1258
habiba <habiba.shata@qeema.net>
2026-07-18 07:20:47 +0300

    add initial login design

--- src/app/features/auth/components/login/login.ts
+++ src/app/features/auth/components/login/login.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AuthLayout } from '../../../../shared/layouts/auth-layout/auth-layout';
-
+import { FormField } from '../../../../shared/components/auth/form-field/form-field';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Button } from 'primeng/button';
 @Component({
   selector: 'app-login',
-  imports: [AuthLayout],
+  imports: [AuthLayout, FormField, Button],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login implements OnInit {
+  private readonly fb = inject(FormBuilder);
+
+  form!: FormGroup;
+  ngOnInit(): void {
+    this.initForm();
+  }
+  initForm() {
+    this.form = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+          ),
+        ],
+      ],
+    });
+  }
+}
