61baae1ce0a4c0952cab5dbbd0e9b0874ae2d640
esraaabuhalawa <esr.saad123@gmail.com>
2026-07-21 23:49:01 +0300

    update student array in edit mode

--- src/app/features/dashboard/instructor/modules/group/components/add-edit-group/add-edit-group.ts
+++ src/app/features/dashboard/instructor/modules/group/components/add-edit-group/add-edit-group.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, inject, Input, Output, signal, SimpleChanges } from '@angular/core';
+import { Component, computed, EventEmitter, inject, Input, Output, signal, SimpleChanges } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TranslatePipe} from '@ngx-translate/core';
 import { DialogModule } from 'primeng/dialog';
@@ -33,8 +33,18 @@ export class AddEditGroup {
 
   allStudents = signal<IStudents[]>([]);
   filteredStudents = signal<IStudents[]>([]);
+  private currentGroup = signal<IGroupData | null>(null);
   loadingStudents = false;
   search = '';
+
+  // Edit mode: current group members first, then students without a group
+  studentOptions = computed(() => {
+    const group = this.currentGroup();
+    if (!group) return this.filteredStudents();
+    const members = this.allStudents().filter(s => group.students.includes(s._id));
+    const unassigned = this.filteredStudents().filter(s => !group.students.includes(s._id));
+    return [...members, ...unassigned];
+  });
 
   constructor() {
     this.formInit();
@@ -58,6 +68,7 @@ export class AddEditGroup {
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['groupData']) {
+      this.currentGroup.set(this.groupData);
       if (this.groupData) {
         this.form.patchValue({
           name: this.groupData.name,
