eed665cf49aab77c95e838541e7588d99985e640
Nourhan-Nabil-9 <nnorhan535@gmail.com>
2026-07-23 00:33:01 +0300

    enhance ui student-list &enhance delete scss

--- src/app/features/dashboard/instructor/modules/students/components/student-list/student-list.ts
+++ src/app/features/dashboard/instructor/modules/students/components/student-list/student-list.ts
@@ -54,7 +54,6 @@ export class StudentList {
   //students after selecting group
   filteredStudents = computed(() => {
     const groupId = this.selectedGroupId();
-    if (!groupId) return this.allStudents();
     return this.allStudents().filter((s) => s.group?._id === groupId);
   });
 
@@ -76,6 +75,9 @@ export class StudentList {
     this.groupsService.getAllGroups().subscribe({
       next: (groups) => {
         this.groupsList.set(groups);
+        if (groups.length > 0) {
+          this.selectedGroupId.set(groups[0]._id);
+        }
       },
     });
   }
